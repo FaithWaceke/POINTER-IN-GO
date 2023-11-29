91631cf036e40f6e4a142e9d208b462e43f7a10a
FaithWaceke <wacekenjosh@gmail.com>
2023-11-29 15:48:20 +0300

    my first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myNum int 
+	var firstNme string 
+	var secondName string
+	var location string
+	var phoneNum int 
+
+	fmt.Println("kindly enter your firstname")
+	fmt.Scan(&firstNme)
+
+	fmt.Println("kindly enter your second name")
+	fmt.Scan(&secondName)
+
+	fmt.Println("where do you reside, enter your location")
+	fmt.Scan(&location)
+
+	fmt.Println("what is your mobile number?")
+	fmt.Scan(&phoneNum)
+
+	fmt.Println("kindly rate me")
+	fmt.Scan(&myNum)
+
+
+
+
+
+}
